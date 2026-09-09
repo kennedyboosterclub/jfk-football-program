a4fd935aba5c583c7df2c1202fb94edd888cf238
kennedyboosterclub <kennedy.booster.treasurer@gmail.com>
2026-09-09 09:21:23 -0500

    Complete team layout update: app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { renderProgram } from "./program-renderer.js";
+import { renderProgram } from "./program-renderer.js?v=20260909-layout2";
 
 const container = document.querySelector("#program");
 const pageCount = document.querySelector("#page-count");
