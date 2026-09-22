ac76e46fa076b7c773c85f8fcfb145f4dcae8f2f
kennedyboosterclub <kennedy.booster.treasurer@gmail.com>
2026-09-22 11:19:49 -0500

    Add configurable action gallery: app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { renderProgram } from "./program-renderer.js?v=20260921-breakfast3";
+import { renderProgram } from "./program-renderer.js?v=20260922-actions1";
 
 const container = document.querySelector("#program");
 const pageCount = document.querySelector("#page-count");
